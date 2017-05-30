5d9ac709d4b34acad3ebbe7e1285f8fbabc3730b
kripton-perm <mixail1024070@mail.ru>
2017-05-30 21:20:54 +0500

    Сетка

--- js/main.js
+++
@@ -1,8 +0,0 @@
-var overlay = document.querySelector(".modal-overlay");
-
-xxx.addEventListener("click", function (event) {
-  event.preventDefault();
-  // Появление попапа yyy при нажатии на элемент xxx
-  yyy.classList.add("modal-feedback-show");
-  overlay.classList.add("modal-overlay-show");
-}
